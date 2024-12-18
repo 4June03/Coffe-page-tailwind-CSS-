0bd6ebe3b4bec8d2e1dfb3f19bdf6a6e9feea629
4June03 <nghia2003hlls@gmail.com>
2024-12-18 11:23:21 +0700

    add files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="root">
+      <div className="content-wrapper">
+        <header>header</header>
+        <main>
+          <div className="slider">slider</div>
+          <div className="story">story</div>
+          <div className="featured-mugs">featured-mugs</div>
+          <div className="more products">more products</div>
+          <div className="coffe magazines">coffe magazines</div>
+          <div className="lifestyle stories">lifestyle stories</div>
+          <div className="subscribes us">subscribes us</div>
+        </main>
+        <footer>footer</footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
